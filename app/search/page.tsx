4c2ac33f01d5551e4d2f353532a5f29bb2b08f42
kabornblack <shaibu.kabiru@email.com>
2024-07-23 03:33:35 +0300

    Search page

---
+++ app/search/page.tsx
@@ -0,0 +1,29 @@
+import Product from "@/components/Product";
+import fetchSearch from "@/lib/fetchSearch";
+
+type Props = {
+  searchParams: {
+    q: String;
+  };
+};
+
+async function SearchPage({ searchParams: { q } }: Props) {
+  const result = await fetchSearch(q);
+
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-2"> Result for {q}</h1>
+      <h2>({result?.content.total_results} results)</h2>
+
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {result?.content.organic.map((product) => (
+          <li key={product.product_id}>
+            <Product product={product} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default SearchPage;
