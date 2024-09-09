e707731ca99df9aaa5b78ce9e34f6a3c2778e04f
kabornblack <shaibu.kabiru@email.com>
2024-09-09 23:34:29 +0300

    Updated to a message if Oxylabs data cannot be reached

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,20 +8,48 @@ type Props = {
 };
 
 async function SearchPage({ searchParams: { q } }: Props) {
+  // Fetch the results from Oxylabs
   const results = await fetchSearch(q);
+
+  // Check if data from Oxylabs exists
+  const hasResults =
+    results && results.content && results.content.organic?.length > 0;
 
   return (
     <div className="p-10 space-y-2">
-      <h1 className="text-3xl font-bold mb-2"> Result for {q}</h1>
-      <h2>({results?.content.total_results} results)</h2>
+      <h1 className="text-3xl font-bold mb-2">Result for {q}</h1>
 
-      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        {results?.content.organic.map((product) => (
-          <li key={product.product_id}>
-            <Product product={product} />
-          </li>
-        ))}
-      </ul>
+      {hasResults ? (
+        <>
+          {/* Display total results */}
+          <h2>({results.content.total_results} results)</h2>
+
+          {/* Display product list */}
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            {results.content.organic.map((product) => (
+              <li key={product.product_id}>
+                <Product product={product} />
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <div className="h-40 w-30 bg-slate-200 text-center p-8">
+          <h2>Couldn&apos;t get results from Oxylabs</h2>
+          <div className="flex items-center justify-center space-x-1 pt-3">
+            <h4 className="text-2xl">⚠️</h4>
+            <h2>Attention needed</h2>
+          </div>
+          {/* Redirect to Oxylabs page */}
+          <a
+            href="https://oxylabs.io/"
+            target="_blank"
+            className="underline text-blue-600 pt-4 block"
+          >
+            Visit Oxylabs for more info
+          </a>
+        </div>
+      )}
     </div>
   );
 }
